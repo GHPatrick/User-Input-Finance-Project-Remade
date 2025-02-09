8acb0ed53cc974f700a64de2e4dd59848c35678b
Patrick Johnson <patrick.johnson7@protonmail.com>
2025-02-09 00:50:41 -0600

    apex updated, lwc classes created and functional

--- force-app/main/default/lwc/investmentCalculator/investmentCalculator.js
+++ force-app/main/default/lwc/investmentCalculator/investmentCalculator.js
@@ -1,3 +1,47 @@
 import { LightningElement } from 'lwc';
+import calculateInvestment from '@salesforce/apex/InvestmentCalculatorController.calculateInvestment';
 
-export default class InvestmentCalculator extends LightningElement {}
+// property declarations
+export default class InvestmentCalculator extends LightningElement {
+    initInv = 0;
+    monDep = 0;
+    anuInt = 0;
+    years = 0;
+
+// arrays to store results and a boolean set to false as default
+    resultsWithoutDeposits = [];
+    resultsWithDeposits = [];
+    showResults = false;
+
+
+// method handling for changes to input fields &
+// data retrieval in context to which field is being updated &
+// conversion of input values
+    handleInputChange(event) {
+        const field = event.target.dataset.id;
+        if (field === 'initInv') {
+            this.initInv = parseFloat(event.target.value);
+        } else if (field === 'monDep') {
+            this.monDep = parseFloat(event.target.value);
+        } else if (field === 'anuInt') {
+            this.anuInt = parseFloat(event.target.value);
+        } else if (field === 'years') {
+            this.years = parseInt(event.target.value);
+        }
+    }
+
+
+// method trigger for when user clicks the Calculate button &
+// calling of the apex class
+    handleCalculate() {
+        calculateInvestment({ initInv: this.initInv, monDep: this.monDep, anuInt: this.anuInt, years: this.years })
+            .then(result => {
+                this.resultsWithoutDeposits = result.withoutDeposits;
+                this.resultsWithDeposits = result.withDeposits;
+                this.showResults = true;
+            })
+            .catch(error => {
+                console.error('Error in calculation:', error);
+            });
+    }
+}
